d2785e6f373817d05caee00aaaff62f28fd42644
AlexJJJChen <126591882+AlexJJJChen@users.noreply.github.com>
2024-05-10 21:36:20 +0800

    Update fintune_llava_7b.sh

--- fintune_llava_7b.sh
+++ fintune_llava_7b.sh
@@ -1,43 +1,16 @@
-# Experimental Environment: 2 * A100
-# GPU Memory Requirement: 10GB + 13GB
-# Runtime: 3.4 hours
-# CUDA_VISIBLE_DEVICES=0,1 \
-# swift sft \
-#     --model_type qwen1half-7b-chat \
-#     --dataset blossom-math-zh \
-#     --num_train_epochs 5 \
-#     --sft_type lora \
-#     --output_dir output \
-
-
-nproc_per_node=4
-CUDA_VISIBLE_DEVICES=0,1,2,3 \
-NPROC_PER_NODE=$nproc_per_node \
+CUDA_VISIBLE_DEVICES=0,1,2,3  
 swift sft \
-    --model_id_or_path "AI-ModelScope/llava-v1.6-mistral-7b" \
-    --template_type "llava-mistral-instruct" \
-    --custom_train_dataset_path wind_gust_dataset/train_swift.json \
-    --custom_val_dataset_path wind_gust_dataset/test_swift.json \
-    --dataset_test_ratio "0.15" \
-    --save_steps "20" \
-    --lora_target_modules q_proj k_proj v_proj \
-    --batch_size "1" \
-    --learning_rate "1e-4" \
-    --num_train_epochs "2" \
-    --gradient_accumulation_steps "16" \
-    --eval_batch_size "1" \
-    --use_flash_attn "True" \
-    --add_output_dir_suffix False \
-    --output_dir test \
-    --logging_dir test\
-    --max_length -1\
-    --train_dataset_sample -1 \
-    --sft_type lora \
-
-    --tuner_backend peft \
-    --quantization_bit 4 \
-    --bnb_4bit_comp_dtype AUTO \
-    --ddp_backend nccl \
-    --check_dataset_strategy warning \
-    --gradient_checkpointing "True" \
-    --deepspeed default-zero3 \
+--sft_type "full" \
+--model_id_or_path "AI-ModelScope/llava-v1.6-mistral-7b" \
+--template_type "llava-mistral-instruct" \
+--custom_train_dataset_path finetune dataset/train.json \
+--custom_val_dataset_path finetune dataset/test.json \
+--save_steps "20" \
+--lora_target_modules c_attn \
+--batch_size "8" \
+--learning_rate "1e-04" \
+--gradient_accumulation_steps "16" \
+--eval_batch_size "8" \
+--add_output_dir_suffix False \
+--output_dir output_llava_7b \
+--logging_dir output_llava_7b \
