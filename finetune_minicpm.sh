daaea5956b9b3507b4d19b8525abb00e8e792115
AlexJJJChen <126591882+AlexJJJChen@users.noreply.github.com>
2024-05-10 21:38:43 +0800

    Add files via upload

---
+++ finetune_minicpm.sh
@@ -0,0 +1,16 @@
+CUDA_VISIBLE_DEVICES=0,1,2,3  
+swift sft \
+--sft_type "full" \
+--model_id_or_path "OpenBMB/MiniCPM-V-2" \
+--template_type "minicpm-v" \
+--custom_train_dataset_path finetune_dataset/train.json \
+--custom_val_dataset_path finetune_dataset/test.json \
+--save_steps "20" \
+--lora_target_modules c_attn \
+--batch_size "8" \
+--learning_rate "1e-04" \
+--gradient_accumulation_steps "16" \
+--eval_batch_size "8" \
+--add_output_dir_suffix False \
+--output_dir output_minicpm \
+--logging_dir output_minicpm \
