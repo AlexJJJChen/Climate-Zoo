d0fed0b1a9466f7020a61ef258b13e51a49e5fd9
AlexJJJChen <126591882+AlexJJJChen@users.noreply.github.com>
2024-05-10 22:15:30 +0800

    Update export_model.sh

--- export_model.sh
+++ export_model.sh
@@ -1,20 +1,8 @@
-CUDA_VISIBLE_DEVICES=0,1,2,3 
-swift export \
-   --ckpt_dir finetune_output/checkpoint-460 --load_dataset_config true \
-   --quant_method awq --quant_bits 4 \
-   --merge_lora true \
-    
-#CUDA_VISIBLE_DEVICES=0,1,2,3 
-#swift deploy \
-#    --ckpt_dir finetune_output/checkpoint-478 --merge_lora true \
-#    --infer_backend vllm --max_model_len 8192
-
-
 #nproc_per_node=4
 CUDA_VISIBLE_DEVICES=0,1,2,3 \
 #NPROC_PER_NODE=$nproc_per_node \
 swift infer \
-    --ckpt_dir finetune_output/checkpoint-460-merged \
+    --ckpt_dir output_internvl/checkpoint-460xxx \
     --load_dataset_config true \
-    --custom_val_dataset_path wind_gust_dataset/test_swift_04.json \
+    --custom_val_dataset_path finetune_dataset/test.json \
     --val_dataset_sample -1\
