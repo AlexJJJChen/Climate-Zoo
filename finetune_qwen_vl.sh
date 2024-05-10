3b0753005f0fb554b6a771ae4cc6f44c3a1859d8
AlexJJJChen <126591882+AlexJJJChen@users.noreply.github.com>
2024-05-10 21:38:20 +0800

    Add files via upload

---
+++ finetune_qwen_vl.sh
@@ -0,0 +1,17 @@
+CUDA_VISIBLE_DEVICES=0,1,2,3  
+swift sft \
+--sft_type "full" \
+--model_id_or_path "qwen/Qwen-VL-Chat" \
+--template_type "qwen" \
+--custom_train_dataset_path finetune_dataset/train.json \
+--custom_val_dataset_path finetune_dataset/test.json \
+--save_steps "20" \
+--lora_target_modules c_attn \
+--batch_size "8" \
+--learning_rate "1e-04" \
+--gradient_accumulation_steps "16" \
+--eval_batch_size "8" \
+--add_output_dir_suffix False \
+--output_dir output_qwen \
+--logging_dir output_qwen \
+
