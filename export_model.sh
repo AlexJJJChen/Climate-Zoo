63719826f292cc533aff17a0fcb99030d2e09f68
AlexJJJChen <126591882+AlexJJJChen@users.noreply.github.com>
2024-05-11 15:30:59 +0800

    Update export_model.sh

--- export_model.sh
+++ export_model.sh
@@ -1,6 +1,6 @@
 #nproc_per_node=4
 CUDA_VISIBLE_DEVICES=0,1,2,3 \
-#NPROC_PER_NODE=$nproc_per_node \
+NPROC_PER_NODE=$nproc_per_node \
 swift infer \
     --ckpt_dir output_internvl/checkpoint-460xxx \
     --load_dataset_config true \
