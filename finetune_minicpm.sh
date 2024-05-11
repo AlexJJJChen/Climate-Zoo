6e8f3e68eedb095b8960d9b19bffe62ff6b1a884
AlexJJJChen <126591882+AlexJJJChen@users.noreply.github.com>
2024-05-11 15:28:15 +0800

    Update finetune_minicpm.sh

--- finetune_minicpm.sh
+++ finetune_minicpm.sh
@@ -1,4 +1,5 @@
-CUDA_VISIBLE_DEVICES=0,1,2,3  
+CUDA_VISIBLE_DEVICES=0,1,2,3 \
+NPROC_PER_NODE=4 \
 swift sft \
 --sft_type "full" \
 --model_id_or_path "OpenBMB/MiniCPM-V-2" \
@@ -6,12 +7,9 @@ swift sft \
 --custom_train_dataset_path finetune_dataset/train.json \
 --custom_val_dataset_path finetune_dataset/test.json \
 --save_steps "20" \
---lora_target_modules c_attn \
 --batch_size "8" \
 --learning_rate "1e-04" \
---gradient_accumulation_steps "16" \
 --eval_batch_size "8" \
---add_output_dir_suffix False \
 --output_dir output_minicpm \
 --logging_dir output_minicpm \
 --num_train_epochs "2" \
